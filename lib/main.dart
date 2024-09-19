e658cc5fe4c3c612bfeff0b227170a63d7e85574
smri29 <22203174@iubat.edu>
2024-09-19 16:47:51 +0600

    1st commit

--- lib/main.dart
+++ lib/main.dart
@@ -28,78 +28,23 @@ class Home extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
-    //Media Query
-    MediaQueryData mediaQuery = MediaQuery.of(context);
-    print(mediaQuery.size.aspectRatio);
-    print(mediaQuery.size.height);
-    print(mediaQuery.size.width);
-    print(mediaQuery.size.shortestSide);
-    print(mediaQuery.size.longestSide);
-    print(mediaQuery.size.flipped);
-    print(mediaQuery.devicePixelRatio);
-
-    if(mediaQuery.size.width < 640){
-      print('This is a Smart Phone');
-    }
-    else if (mediaQuery.size.width > 640 && mediaQuery.size.width <1080){
-      print('This is a Tablet');
-    }
-    else{
-      print('This is a TV');
-    }
-
     return Scaffold(
       appBar: AppBar(
         title: Text('Welcome'),
         backgroundColor: Colors.purpleAccent,
       ),
-      /*body: OrientationBuilder(
-        builder: (context, Orientation orientation) {
-          print(orientation);
-          if (orientation == Orientation.portrait){
-            return Column(
-              children: [
-                Wrap(
-                  alignment: WrapAlignment.spaceEvenly,
-                  crossAxisAlignment: WrapCrossAlignment.center,
-                  spacing: 6,
-                  children: [
-                    Text('Roman Reigns'),
-                    Text('Dean Ambrose'),
-                    Text('Seth Rollins'),
-                    Text('John Cena'),
-                    Text('Triple H'),
-                    Text('Dwayne Rock'),
-                    Text('Undertaker'),
-                    Text('Stone Cold Steve Austin'),
-                    Text('Jacob Fatu'),
-                    Text('Solo Sikoa'),
-                  ],
-                ),
-                Text(orientation.name),
-              ],
-            );
-          }
-          else{
-            return Center(
-              child: Text('To Big Screen'),
-            );
-          }
-        }
-      ),*/
-      body: LayoutBuilder
-        (builder: (context, BoxConstraints constraints){
-        if(constraints.maxWidth < 640){
-          return Text('This is a Smart Phone');
-        }
-        else if (constraints.maxWidth > 640 && constraints.maxWidth <1080){
-          return Text('This is a Tablet');
-        }
-        else{
-          return Text('This is a TV');
-        }
-      }
+      body: Container(
+        height: MediaQuery.of(context).size.height,
+        width: MediaQuery.of(context).size.width,
+        color: Colors.blueGrey,
+        child: FractionallySizedBox(
+          widthFactor: 0.5,
+          heightFactor: 0.5,
+          child: Container(
+            color: Colors.white54,
+          ),
+        ),
       ),
-    );
+    );// scaffold
   }
 }
